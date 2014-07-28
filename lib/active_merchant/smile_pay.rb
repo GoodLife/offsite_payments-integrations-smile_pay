58ff3831c417eb0ee1ab359eb87a889e8a1095d6
lulalala <mark@goodlife.tw>
2014-07-28 18:23:24 +0800

    Move files to new locations; Merge smile_pay.rb base file with actual code file

--- lib/active_merchant/smile_pay.rb
+++
@@ -1,3 +0,0 @@
-require "active_merchant/smile_pay/version"
-require "active_merchant"
-require "active_merchant/billing/integrations/smile_pay"
--- lib/offsite_payments/integrations/smile_pay.rb
+++ lib/offsite_payments/integrations/smile_pay.rb
@@ -1,3 +1,6 @@
+require "active_merchant/smile_pay/version"
+require "active_merchant"
+require "active_merchant/billing/integrations/smile_pay"
 require 'net/http'
 
 module ActiveMerchant #:nodoc:
